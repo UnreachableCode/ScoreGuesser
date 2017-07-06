01bc2d84e2d2bb203172c10047f30555ba6baece
UnreachableCode <cfin@protonmail.com>
2017-07-06 21:12:29 +0100

    Player object

---
+++ ScoreGuesser.iOS/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Foundation;
+using UIKit;
+
+namespace ScoreGuesser.iOS
+{
+    public class Player
+    {
+        public Player(string first_Name, string last_Name, string imageUrl, string fppg)
+        {
+            First_Name = first_Name;
+            Last_Name = last_Name;
+            ImageUrl = imageUrl;
+            FPPG = fppg;
+        }
+
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string FPPG { get; set; }
+
+        public async Task<UIImage> LoadImage()
+        {
+            var httpClient = new HttpClient();
+
+            Task<byte[]> contentsTask = httpClient.GetByteArrayAsync(ImageUrl);
+
+            var contents = await contentsTask;
+
+            return UIImage.LoadFromData(NSData.FromArray(contents));
+        }
+    }
+}
