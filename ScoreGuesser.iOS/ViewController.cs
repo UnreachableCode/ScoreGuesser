17367f2875b2e5c0e7d76030c5de66c957142a60
UnreachableCode <cfin@protonmail.com>
2017-07-06 21:14:48 +0100

    PlayerDataDownloader class implemented

--- ScoreGuesser.iOS/ViewController.cs
+++ ScoreGuesser.iOS/ViewController.cs
@@ -1,26 +1,28 @@
 ﻿using System;
-
 using UIKit;
+using System.Collections.Generic;
+using Foundation;
 
-namespace ScoreGuesser
+namespace ScoreGuesser.iOS
 {
     public partial class ViewController : UIViewController
     {
-        protected ViewController(IntPtr handle) : base(handle)
-        {
-            // Note: this .ctor should not contain any initialization logic.
-        }
+        PlayerDataDownloader _playerDataDownloader;
+        List<Player> _playerList;
 
-        public override void ViewDidLoad()
+        protected ViewController(IntPtr handle) : base(handle) { }
+
+        public override async void ViewDidLoad()
         {
             base.ViewDidLoad();
-            // Perform any additional setup after loading the view, typically from a nib.
-        }
 
-        public override void DidReceiveMemoryWarning()
-        {
-            base.DidReceiveMemoryWarning();
-            // Release any cached data, images, etc that aren't in use.
+            string url = @"https://cdn.rawgit.com/liamjdouglas/bb40ee8721f1a9313c22c6ea0851a105/raw/6b6fc89d55ebe4d9b05c1469349af33651d7e7f1/Player.json";
+
+            _playerDataDownloader = new PlayerDataDownloader(url);
+
+            _playerList = await _playerDataDownloader.FetchPlayersDataAsync(url);
         }
     }
 }
+
+
